db756e659b3010191f7a0eb55182dd06f0a59f28
Kallen Lonbom <lonbom@chapman.edu>
2024-10-04 15:05:50 -0700

    Added user input

--- generator.py
+++ generator.py
@@ -3,9 +3,10 @@ from llama_index.llms.openai import OpenAI
 from llama_index.core.node_parser import SimpleNodeParser
 import os
 from importlib import reload
+import translate
 import sys
 
-os.environ["OPENAI_API_KEY"] = "YOUR API KEY"
+os.environ["OPENAI_API_KEY"] = "API KEY"
 
 examples = SimpleDirectoryReader(input_files=['data/train.csv']).load_data()
 print("Loaded")
@@ -19,15 +20,102 @@ node_parser = SimpleNodeParser.from_defaults(chunk_size=260,chunk_overlap=2)
 nodes = node_parser.get_nodes_from_documents(examples)
 index = VectorStoreIndex(nodes)
 writer = index.as_chat_engine()
-try:
-    import test
-except:
-    open('test.py', 'w')
-    import test
 
-def Test(sampleInputs, sampleOutputs):
+def GenerateExample():
+    example = 'case\ninputs\nstr:volcano\nint:4\noutputs\nstr:volc\ncase\ninputs\nstr:word\nint:2\noutputs\nstr:wo'
+    with open('example.txt', 'w') as file:
+        file.write(example)
+
+def ParseTest(filepath):
+    inputs = []
+    outputs = []
+    with open(filepath, 'r') as file:
+        phase = 'case'
+        index = 0
+        for line in file:
+            line = line.rstrip()
+            if phase == 'case':
+                if line != 'case':
+                    print('"'+line+'"')
+                    raise Exception('Invalid test file format')
+                inputs.append([])
+                outputs.append([])
+                phase = 'inputs'
+                continue
+            if phase == 'inputs':
+                if line == 'inputs':
+                    continue
+                if line == 'outputs':
+                    phase = line
+                    continue
+                var = line.split(':')[0]
+                value = line.split(':')[1]
+                if var == 'str':
+                    inputs[index].append(value)
+                elif var == 'int':
+                    inputs[index].append(int(value))
+                elif var == 'bool':
+                    if value == 'True':
+                        inputs[index].append(True)
+                    elif value == 'False':
+                        inputs[index].append(False)
+                    else:
+                        raise Exception('Invalid test file format')
+                elif var == 'float':
+                    inputs[index].append(float(value))
+                elif var == 'char':
+                    inputs[index].append(value[0])
+            if phase == 'outputs':
+                if line == 'case':
+                    phase = 'inputs'
+                    index += 1
+                    inputs.append([])
+                    outputs.append([])
+                    continue
+                var = line.split(':')[0]
+                value = line.split(':')[1]
+                if var == 'str':
+                    outputs[index].append(value)
+                elif var == 'int':
+                    outputs[index].append(int(value))
+                elif var == 'bool':
+                    data = line.split(':')[1]
+                    if data == 'True':
+                        outputs[index].append(True)
+                    elif data == 'False':
+                        outputs[index].append(False)
+                    else:
+                        raise Exception('Invalid test file format')
+                elif var == 'float':
+                    outputs[index].append(float(value))
+                elif var == 'char':
+                    outputs[index].append(value[0])
+    return (inputs, outputs)
+
+inputs, outputs = ParseTest('example.txt')
+print(inputs)
+print(outputs)
+
+ParseTest('example.txt')
+
+def Test(values):
+    testFunction = 'TestFunction('
+    index = 0
+    for value in inputs:
+        testFunction += 'inputs[' + str(index) + '],'
+        index += 1
+    testFunction = testFunction[0:-1] + ')'
+    firsthalf = 'from test import TestFunction\ndef Test(inputs):\n  print(inputs)\n  output = '
+    secondhalf = '\n  return output'
+    with open('translate.py', 'w') as translator:
+        translator.write(firsthalf)
+        translator.write(testFunction)
+        translator.write(secondhalf)
+    reload(translate)
+    sampleInputs, sampleOutputs = values
+    print(sampleInputs)
     try:
-        realOutputs = test.TestFunction(sampleInputs)
+        realOutputs = translate.TestFunction(sampleInputs)
         print(realOutputs)
         if realOutputs == sampleOutputs:
             return False
@@ -41,18 +129,23 @@ def Test(sampleInputs, sampleOutputs):
         except:
             return 'Line ' + str(line_number)+ ': ' + str(ex) + ' Rewrite the entire corrected script.'
     
-#input = input('Write a function that:')
-input = 'Write a python script that has one function called "TestFunction" which ' + ''
-while True:
-    print(input)
-    output = writer.chat(input)
-    print(output)
-    code = str(output).split('```')[1][6:]
-    with open('test.py', 'w') as file:
-        file.write(code)
-    reload(test)
-    failed = Test('https://github.com/ollama/ollama', None)
+prompt1 = input('Write a function that, given (describe inputs): ')
+prompt2 = input('returns (describe outputs): ')
+prompt = 'Write a python script that has one function called "TestFunction" which given ' + prompt1 + ', returns ' + prompt2
+test_cases = ''
+#while True:
+print(prompt)
+output = writer.chat(prompt)
+print(output)
+code = str(output).split('```')[1][6:]
+with open('test.py', 'w') as file:
+    file.write(code)
+if test_cases == '':
+    test_cases = input('\nEnter name of test case file, or "help" to generate an example test file, or enter to skip test phase (not recommended unless testable inputs and outputs are not practical): ')
+if test_cases == 'help':
+    GenerateExample()
+tests = ParseTest(test_cases)
+for case in tests:
+    failed = Test(case)
     if failed:
-        input = 'Error: ' + failed
-    else:
-        break
+        prompt = 'Error: ' + failed
--- test.py
+++ test.py
@@ -1,29 +1,18 @@
 
-import os
-import requests
-from bs4 import BeautifulSoup
-from urllib.parse import urljoin
+def TestFunction(acceptable_inputs):
+    while True:
+        user_input = input("Please enter your input: ")
+        if acceptable_inputs:
+            if user_input in acceptable_inputs:
+                return user_input
+            else:
+                print("Invalid input. Please try again.")
+        else:
+            try:
+                return int(user_input)
+            except ValueError:
+                print("Invalid input. Please enter a number.")
 
-def TestFunction(url):
-    if not os.path.exists('output'):
-        os.makedirs('output')
-
-    to_visit = [url]
-    visited = set()
-
-    while to_visit:
-        current_url = to_visit.pop(0)
-        visited.add(current_url)
-
-        response = requests.get(current_url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-
-        with open('output/' + current_url.replace('http://', '').replace('https://', '').replace('/', '_') + '.html', 'w', encoding='utf-8') as f_out:
-            f_out.write(response.text)
-
-        for link in soup.find_all('a'):
-            absolute_link = urljoin(current_url, link.get('href'))
-            if absolute_link.startswith(url) and absolute_link not in visited:
-                to_visit.append(absolute_link)
-
-TestFunction('http://example.com')
+# Test the function
+print(TestFunction(['apple', 'banana', 'cherry']))
+print(TestFunction([]))
